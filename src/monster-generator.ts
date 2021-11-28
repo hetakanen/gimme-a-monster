1e3619610fc80402d442ca1d860fe02d5f504ba2
Pipo Soma <applepiefever$@gmail.com>
2021-11-28 23:40:02 +0200

    feat: Add generate monster

---
+++ src/monster-generator.ts
@@ -0,0 +1,54 @@
+import * as colors from "./data/color-options.json";
+import * as shapes from "./data/shape-options.json";
+import * as mouths from "./data/mouth-options.json";
+import * as eyes from "./data/eye-options.json";
+import * as monsters from "./data/monster-options.json";
+import { getRandomValue, getRandomNumber } from "./random-value-generator";
+import { Complexity, IHighMonster, IMediumMonster, IMonster } from "./types";
+
+export const generate = (complexity: Complexity = "low") => {
+  let result: IMonster = {
+    color: "",
+    type: "",
+    stringify: "",
+  };
+
+  if (complexity === "high") {
+    addEyes(result as IHighMonster);
+  }
+
+  if (complexity === "high" || complexity === "medium") {
+    addMouth(result as IMediumMonster);
+    addShape(result as IMediumMonster);
+  }
+
+  addColor(result);
+  addType(result);
+  return result;
+};
+
+const addEyes = (result: IHighMonster) => {
+  result.eyeAmount = getRandomNumber(10);
+  result.eyes = getRandomValue(eyes) as string;
+  result.stringify += result.eyeAmount + " " + result.eyes + " ";
+};
+
+const addMouth = (result: IMediumMonster) => {
+  result.mouth = getRandomValue(mouths);
+  result.stringify += result.mouth + " ";
+};
+
+const addShape = (result: IMediumMonster) => {
+  result.shape = getRandomValue(shapes);
+  result.stringify += result.shape + " ";
+};
+
+const addColor = (result: IMonster) => {
+  result.color = getRandomValue(colors);
+  result.stringify += result.color + " ";
+};
+
+const addType = (result: IMonster) => {
+  result.type = getRandomValue(monsters);
+  result.stringify += result.type;
+};
