fbb7c71e4f799bd44784a9de6fd874d2ebd5de4e
Pipo Soma <applepiefever$@gmail.com>
2021-11-28 23:39:42 +0200

    feat: Add generate random values

---
+++ src/random-value-generator.ts
@@ -0,0 +1,16 @@
+
+/**
+ * @param values
+ * @returns Random value from @values using Math.random
+ */
+export const getRandomValue = (values: Array<any>) => {
+  return values[Math.floor(Math.random() * values.length)];
+};
+
+/**
+ * @param max
+ * @returns Get value between @max and 0
+ */
+export const getRandomNumber = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
