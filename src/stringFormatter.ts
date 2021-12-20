f42e70bed23ae73eadca55285ff1c637d22e42cb
Pipo Soma <applepiefever$@gmail.com>
2021-12-20 21:54:14 +0200

    feat: Add string formatter

---
+++ src/stringFormatter.ts
@@ -0,0 +1,18 @@
+import { Casing, IMonsterProps } from "./types";
+
+const capitalize = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export const format = (str: string, casing?: Casing) => {
+  switch (casing) {
+    case "capitalize":
+      return capitalize(str);
+    case "upper":
+      return str.toUpperCase();
+    case "lower":
+      return str.toLocaleLowerCase();
+    default:
+      return str;
+  }
+};
