b6789df20ed38ed8d5d1ec9b4b2a05cb1977009f
Pipo Soma <applepiefever$@gmail.com>
2022-01-09 22:09:56 +0200

    refactor: Allow typing for values with default

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     rules: {
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off'
     },
   };
   
