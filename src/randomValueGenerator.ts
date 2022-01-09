c14f7e50953fa7931165c2a1e0183f717119a471
Pipo Soma <applepiefever$@gmail.com>
2022-01-09 22:06:39 +0200

    refactor: Run prettier

--- src/randomValueGenerator.ts
+++ src/randomValueGenerator.ts
@@ -11,6 +11,6 @@ export const getRandomValue = (values: Array<string>) => {
  * @param min
  * @returns Get value between @max and @min
  */
- export const getRandomNumber = (max: number, min: number = 0) => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+export const getRandomNumber = (max: number, min: number = 0) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
